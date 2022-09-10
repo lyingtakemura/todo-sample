18ab570071db4589238807dde256a2d98752a717
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-09-10 13:07:53 +0300

    Add todo viewset

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from todos.views import TodoViewSet
+
+router = routers.SimpleRouter()
+router.register(r'todos', TodoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls))
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+from todos.serializers import TodoSerializer
+from todos.models import Todo
+
+
+class TodoViewSet(
+    viewsets.GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin
+):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
