d4b9022fd7981358e6b80e1974171a54dfcf8ee5
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-09-12 17:15:09 +0300

    Get todos with axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    todos: []
+  };
+
+  componentDidMount() {
+    this.getTodos();
+  }
+
+  getTodos() {
+    axios
+    .get('http://127.0.0.1:8000/api/v1/todos/')
+    .then(response => {
+      this.setState({ todos: response.data });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  handleCheckClick(checked) {
+    console.log(checked)
+    const todos = this.state.todos.map((item) => {
+      if (item.id === checked.id) {
+        const updatedItem = {
+          ...item,
+          done: !item.done,
+        };
+
+        return updatedItem;
+      }
+
+      return item;
+    });
+
+    this.setState({ todos: todos });
+  }
+    
+
+  render() {
+    return (
+      <div>
+      {this.state.todos.map(item => (
+        <div className={`flex font-semibold bg-slate-300 antialiased w-1/3 mx-auto my-1 border-2 p-4 border-slate-400 rounded-md ${item.done ? 'border-green-400' : 'border-slate-400'} `} key={item.id}>
+          <div className='w-3/4 inline-block align-middle'>{item.title}</div>
+          <span className='w-1/4 flex justify-end '>
+            <input type="checkbox" className={``} onChange={() => this.handleCheckClick(item)} defaultChecked={item.done} />
+          </span>
+        </div>
+      ))}
+      </div>
+      );
+    }
+  }
 
 export default App;
