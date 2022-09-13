90dffa552ddb4499cd35281006c413b916962109
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-09-13 16:21:30 +0300

    Delete todo with func via axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,9 @@ import './App.css';
 import axios from "axios";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [],
-      input: ''
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  state = {
+    todos: [],
+    input: ''
   }
 
   componentDidMount() {
@@ -31,10 +25,10 @@ class App extends Component {
     });
   }
 
-  handleCheckClick(checked) {
-    axios.patch('http://127.0.0.1:8000/api/v1/todos/' + checked.id + '/',
+  handleCheckbox(obj) {
+    axios.patch('http://127.0.0.1:8000/api/v1/todos/' + obj.id + '/',
       {
-        "done": !checked.done
+        "done": !obj.done
       },
     )
     .then((response) => {
@@ -60,25 +54,33 @@ class App extends Component {
     })
   }
 
-  handleChange(event) {
-    console.log(event.target.value)
+  handleInput(event) {
     this.setState({
       input: event.target.value
+    })
+  }
+
+  handleDelete(obj) {
+    axios.delete(
+      'http://127.0.0.1:8000/api/v1/todos/' + obj.id + '/'
+    ).then(() => {
+      this.getTodos()
     })
   }
 
   render() {
     return (
       <div className='font-semibold antialiased'>
-        <form onSubmit={this.handleSubmit} className='bg-slate-300 w-1/3 mx-auto my-1 rounded-md'>
-          <input type="text" required placeholder='TODO' value={this.state.input} onChange={this.handleChange} className='w-5/6 border-2 p-4 border-slate-400 rounded-md' />
+        <form onSubmit={this.handleSubmit.bind(this)} className='bg-slate-300 w-1/3 mx-auto my-1 rounded-md'>
+          <input type="text" required placeholder='TODO' value={this.state.input} onChange={this.handleInput.bind(this)} className='w-5/6 border-2 p-4 border-slate-400 rounded-md' />
           <input type="submit" value="ADD" className='border-2 p-4 border-slate-400 rounded-md w-1/6 hover:bg-green-400' />
         </form>
         {this.state.todos.map(item => (
         <div className={`flex bg-slate-300 antialiased w-1/3 mx-auto my-1 border-2 p-4 border-slate-400 rounded-md ${item.done ? 'border-green-400' : 'border-slate-400'} `} key={item.id}>
-          <div className='w-3/4'>{item.title}</div>
+          <div className='w-3/4'>
+            {item.title} <span onClick={this.handleDelete.bind(this, item)} className='text-xs hover:text-red-500'>(DELETE)</span></div>
           <span className='w-1/4 flex justify-end '>
-            <input type="checkbox" className={``} onChange={() => this.handleCheckClick(item)} defaultChecked={item.done} />
+            <input type="checkbox" onChange={this.handleCheckbox.bind(this, item)} defaultChecked={item.done} />
           </span>
         </div>
         ))}
