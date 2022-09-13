852d8d66ee75dd83c579422ac02c2bb01b721f15
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-09-13 15:30:55 +0300

    Add todo input form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,16 @@ import './App.css';
 import axios from "axios";
 
 class App extends Component {
-  state = {
-    todos: []
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      input: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   componentDidMount() {
     this.getTodos();
@@ -15,15 +22,16 @@ class App extends Component {
     axios
     .get('http://127.0.0.1:8000/api/v1/todos/')
     .then(response => {
-      this.setState({ todos: response.data });
+      this.setState({
+        todos: response.data
+      })
     })
     .catch(error => {
-      console.log(error);
+      console.log(error)
     });
   }
 
   handleCheckClick(checked) {
-    // console.log(checked)
     axios.patch('http://127.0.0.1:8000/api/v1/todos/' + checked.id + '/',
       {
         "done": !checked.done
@@ -31,38 +39,49 @@ class App extends Component {
     )
     .then((response) => {
       this.getTodos()
-        // const todos = this.state.todos.map((item) => {
-        //   if (item.id === checked.id) {
-        //     const updatedItem = {
-        //       ...item,
-        //       done: !item.done,
-        //     };
-    
-        //     return updatedItem;
-        //   }
-    
-        //   return item;
-        // });
-    
-        // this.setState({ todos: todos });
     })
     .catch((error) => {
       console.log(error)
     })
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+    axios.post("http://127.0.0.1:8000/api/v1/todos/", {
+      title: this.state.input,
+    }).then(() => {
+      this.setState({
+        input: ""
+      })
+      this.getTodos()
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  handleChange(event) {
+    console.log(event.target.value)
+    this.setState({
+      input: event.target.value
+    })
+  }
 
   render() {
     return (
-      <div>
-      {this.state.todos.map(item => (
-        <div className={`flex font-semibold bg-slate-300 antialiased w-1/3 mx-auto my-1 border-2 p-4 border-slate-400 rounded-md ${item.done ? 'border-green-400' : 'border-slate-400'} `} key={item.id}>
-          <div className='w-3/4 inline-block align-middle'>{item.title}</div>
+      <div className='font-semibold antialiased'>
+        <form onSubmit={this.handleSubmit} className='bg-slate-300 w-1/3 mx-auto my-1 rounded-md'>
+          <input type="text" required placeholder='TODO' value={this.state.input} onChange={this.handleChange} className='w-5/6 border-2 p-4 border-slate-400 rounded-md' />
+          <input type="submit" value="ADD" className='border-2 p-4 border-slate-400 rounded-md w-1/6 hover:bg-green-400' />
+        </form>
+        {this.state.todos.map(item => (
+        <div className={`flex bg-slate-300 antialiased w-1/3 mx-auto my-1 border-2 p-4 border-slate-400 rounded-md ${item.done ? 'border-green-400' : 'border-slate-400'} `} key={item.id}>
+          <div className='w-3/4'>{item.title}</div>
           <span className='w-1/4 flex justify-end '>
             <input type="checkbox" className={``} onChange={() => this.handleCheckClick(item)} defaultChecked={item.done} />
           </span>
         </div>
-      ))}
+        ))}
       </div>
       );
     }
